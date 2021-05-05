720632228143f49468f63192ab8837a87b6e678d
Minde339 <58472290+Minde339@users.noreply.github.com>
2021-05-05 22:44:19 +0300

    Java Static initializer solved

---
+++ Java Static Initializer Block/src/com/company/Main.java
@@ -0,0 +1,53 @@
+//Static initialization blocks are executed when the class is loaded, and you can initialize static variables in those blocks.
+//
+//It's time to test your knowledge of Static initialization blocks. You can read about it here.
+//
+//You are given a class Solution with a main method.
+// Complete the given code so that it outputs the area of a parallelogram with breadth B and height H.
+//You should read the variables from the standard input.
+//
+//If B <= 0 or H <= 0 , the output should be
+// "java.lang.Exception: Breadth and height must be positive" without quotes.
+//
+//Input Format
+//
+//There are two lines of input. The first line contains B : the breadth of the parallelogram.
+// The next line contains H : the height of the parallelogram.
+//
+//Constraints
+//
+//Output Format
+//
+//If both values are greater than zero, then the main method must output the area of the parallelogram.
+// Otherwise, print "java.lang.Exception: Breadth and height must be positive" without quotes.
+
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    static int B;
+    static int H;
+    static boolean flag = true;
+    static {
+        Scanner input = new Scanner(System.in);
+        B = input.nextInt();
+        H = input.nextInt();
+        input.close();
+        try{
+            if(B <= 0 || H <= 0) {
+                flag = false;
+                throw new Exception("Breadth and height must be positive");
+            }
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        if(flag){
+            int area=B*H;
+            System.out.print(area);
+        }
+    }
+}
